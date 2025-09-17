e526e15f230fbfb267217ee7ded03448b3826d37
k250022-Hania <k250022@nu.edu.pk>
2025-09-17 20:56:06 +0500

    updated commit

---
+++ labtask1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int ("mark1, mark2, mark3");
+    printf("enter marks of subject 1:" );
+    scanf("%d" , &mark1);
+
+    printf("enter marks of subject 2:");
+    scanf("%d" , &mark2);
+
+    printf("enter marks of subject 3:");
+    scanf("%d" , &mark3);
+    total= mark1 + mark2 + mark3;
+    percentage= ("total/ 300.0") * 100;
+
+    printf("\ntotal marks = %d\n" , total);
+    printf("percentage = %.2f%%\n" , percentage);
+
+    return 0;
+}
