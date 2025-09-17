1d330c039a0ded671f4f563681d94fc9bb0e6376
k250022-Hania <k250022@nu.edu.pk>
2025-09-17 20:59:58 +0500

    updated commit

---
+++ labtask5.c
@@ -0,0 +1,10 @@
+#inlude <stdio.h>
+
+int main (){
+    printf("Name: Ali\n");
+    printf("Age: 20\n");
+    printf("Course: \"C Proramming\"\n");
+    printf("Result: Passed\n");
+
+    return 0;
+}
